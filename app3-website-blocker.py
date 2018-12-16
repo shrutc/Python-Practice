1d638cd40e8db304573f4bdcfead19cb8faf3d65
Shruti Chandrasekhar <41589619+shrutc@users.noreply.github.com>
2018-12-16 14:50:52 -0800

    Update app3-website-blocker.py

--- app3-website-blocker.py
+++ app3-website-blocker.py
@@ -1,11 +1,18 @@
 import time
 from datetime import datetime as dt
+# we are importing the datetime module to tell the program the time from which the scheduler needs to start.
+
 
 hosts_temp=r"hosts"
-hosts_path=r"C:\Windows\System32\drivers\etc\hosts"
+# its a good practice to save the folder in a temporary drive. so that first test it on that before implementing the actual code.
+hosts_path=r"C:\Windows\System32\drivers\etc\hosts" # this is the actual path where the host file is saved.
+# make sure to open the actual host file to check what all sites actually exist.
 redirect="127.0.0.1"
-website_list=["www.facebook.com","facebook.com","dub119.mail.live.com","www.dub119.mail.live.com"]
+website_list=["www.facebook.com","facebook.com","twitter.com","www.twitter.com"]
 
+# setting up an infinite loop inside the program.
+# setting up the time, when you want to block the websites.
+# opening the host file in read and write mode and adding the websites name if it is not present.
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Working hours...")
